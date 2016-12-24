71216f8ac1aed9f6562d9dc0a1b9f5d074048e12
CodingXiang <dryadf66449@gmail.com>
2016-12-25 03:17:08 +0800

    分隔為 ＭＶＣ，並且加入註解

--- controller.js
+++ controller.js
@@ -1,41 +1,13 @@
-// firebase
-var config = {
-  apiKey: "AIzaSyBdydZ_9C9gKI4aRX7YSXYQKL4Nvk54dok",
-  authDomain: "cloudtalk-e3181.firebaseapp.com",
-  databaseURL: "https://cloudtalk-e3181.firebaseio.com",
-  storageBucket: "cloudtalk-e3181.appspot.com",
-  messagingSenderId: "53484565436"
-};
-firebase.initializeApp(config);
-function sendMessage(username, message, date) {
-  var postData = {
-    sender: username,
-    message : message,
-    time : date
-  };
-
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('posts').push().key;
-
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/chats/' + newPostKey] = postData;
-
-  return firebase.database().ref().update(updates);
-}
-
-
-
-// angularjs
-var app = angular.module("testapp",[]);
-
-app.factory("DataModel", function() {
-  var Service = {};
-  return Service;
-});
-
+/*
+  AngularJS 部分
+*/
+// 設定 ng-app 名稱為 FirebaseChat
+var app = angular.module("FirebaseChat",[]);
+// 設定 FirebaeChat 的 Controller 名為 ChatController
 app.controller("ChatController", function($scope) {
+  // chatMessages 為訊息陣列
   $scope.chatMessages = [];
+  /*  這裡用來讀取 Firebase 中的聊天訊息 */
   var chatData = firebase.database().ref('chats');
   chatData.on('value', function(snapshot) {
     angular.element(document.getElementById('test')).scope().chatMessages = []
@@ -50,7 +22,7 @@ app.controller("ChatController", function($scope) {
     }
   });
 
-
+  // 聊天訊息的格式
   $scope.formatChat = function(username,text,origDt) {
     console.log(origDt)
     var chat = {};
@@ -59,18 +31,18 @@ app.controller("ChatController", function($scope) {
     chat.time = origDt;
     return chat;
   }
-
+  // 新增聊天訊息到 html 當中
   $scope.addMessage = function(username,text,date){
     var chat = $scope.formatChat(username,text,new Date(date));
     $scope.chatMessages.push(chat)
   }
+  // 新增訊息到 Firebase 當中
   $scope.addChat = function() {
     if ($scope.newChatMsg != "" && $scope.username != "") {
       sendMessage($scope.username , $scope.newChatMsg , new Date().toString())
       $scope.newChatMsg = "";
-    }
-    else {
-
+    }else{
+      alert("請輸入完整訊息！")
     }
   }
 
---
+++ model.js
@@ -0,0 +1,26 @@
+// Firebase 初始化
+var config = {
+  apiKey: "AIzaSyBdydZ_9C9gKI4aRX7YSXYQKL4Nvk54dok",
+  authDomain: "cloudtalk-e3181.firebaseapp.com",
+  databaseURL: "https://cloudtalk-e3181.firebaseio.com",
+  storageBucket: "cloudtalk-e3181.appspot.com",
+  messagingSenderId: "53484565436"
+};
+firebase.initializeApp(config);
+
+// 將聊天資料上傳至 Firebase 當中
+function sendMessage(username, message, date) {
+  var postData = {
+    sender: username,
+    message : message,
+    time : date
+  };
+
+  // 取得亂數 key
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  var updates = {};
+  updates['/chats/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
